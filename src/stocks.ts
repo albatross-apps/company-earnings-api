18c14f58ad6a2fe3e1371ad2ed8ae85c9a0cfd01
AG <champtoday@gmail.com>
2022-05-09 23:29:26 -0400

    🌟 Add all files

--- src/stocks.ts
+++ src/stocks.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import { join } from "path";
 import os from "os";
 import { ReportResp, TagsObject } from "./utils/types";
+import cliProgress from "cli-progress";
 
 const fsPromise = fs.promises;
 
@@ -11,17 +12,30 @@ const parse = async (fileName: string) => {
   return await JSON.parse(contents.toString());
 };
 
-const getTags = async () => {
-  const data = (await parse(
-    join(os.homedir() + "/Downloads/companyfacts/CIK0000002491.json")
-  )) as ReportResp;
+const getTags = async (fileName: string) => {
+  const data = (await parse(join(fileName))) as ReportResp;
 
-  return data.facts["us-gaap"] as TagsObject;
+  return (data.facts?.["us-gaap"] as TagsObject) ?? undefined;
 };
 
 const main = async () => {
-  const tags = await getTags();
-  console.log(tags.AssetsCurrent.units.USD);
+  const base = join(os.homedir() + "/Downloads/companyfacts");
+  const files = await fsPromise.readdir(base);
+  const map: Record<string, any> = {};
+  const bar1 = new cliProgress.SingleBar(
+    {},
+    cliProgress.Presets.shades_classic
+  );
+  bar1.start(files.length, 0);
+  let i = 0;
+  for (const file of files) {
+    const tags = await getTags(join(base, "/", file));
+    if (tags) map[file] = tags.Assets?.label;
+    bar1.update(i);
+    i++;
+  }
+  bar1.stop();
+  console.log("Values: ", Object.keys(map).length);
 };
 
 main();
